e71ec159f9bf7fccb3b2a5643f1033ded5fc1226
Jonathan Tredway <jptredway@gmail.com>
2018-09-19 14:46:07 -0500

    Add LiveFeed component

---
+++ client/src/components/LiveFeed.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class LiveFeed extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello from live feed</h1>
+    );
+  }
+}
+
+export default LiveFeed;
