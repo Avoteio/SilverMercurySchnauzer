ba995465e112f4b09d7dee0d128723737c658fe2
Hunter Leeves <hunterleeves@gmail.com>
2018-09-21 14:52:32 -0500

    working tone calls to watson

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -20,6 +20,7 @@ class Dashboard extends React.Component {
 
     this.handleValidation = this.handleValidation.bind(this);
     this.populateFeed = this.populateFeed.bind(this);
+    this.getUserTone = this.getUserTone.bind(this);
   }
 
   componentDidMount() {
@@ -28,7 +29,19 @@ class Dashboard extends React.Component {
     }, () => {
       this.handleValidation();
     });
+
+    this.getUserTone();
   }
+
+  getUserTone() {
+    axios.get(`/api/users/${localStorage.getItem('userId')}/getUserTone`)
+    .then(({data}) => {
+      console.log('talking to user tweets for those sweet, dulcet tones!',data)
+    })
+    .catch(console.log);
+  }
+
+  
 
   handleValidation() {
     axios.post('/validateuser', {
